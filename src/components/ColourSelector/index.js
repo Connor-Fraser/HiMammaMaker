b567d72753082d57a94937ec17efa9b1c45c7c42
Connor <connor.fraser4@gmail.com>
2019-07-24 17:49:23 -0400

    Add Colour Selection Widget to component

--- src/components/ColourSelector/index.js
+++ src/components/ColourSelector/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import CONSTS from '../../utils/constants.js'
+import { TwitterPicker } from 'react-color';
 import './ColourSelector.css';
 
 /*
@@ -11,17 +13,50 @@ export default class ColourSelector extends React.Component {
         super(props);
 
         this.state = {
-            colour: this.props.avatarComponent.DEFAULT_COLOUR
+            colour: this.props.avatarComponent.DEFAULT_COLOUR,
+            clicked: false
         }
-    };
+
+        this._clickHandler = () => {
+            this.setState({
+                clicked: true
+            });
+        }
+
+        this._colourChangeHandler = (newColour) => {
+            this.setState({
+                colour: newColour.hex,
+                clicked: false
+            });
+        }
+
+        this._colourHoverHandler = (newColour) => {
+            this.setState({
+                colour: newColour.hex
+            });
+        }
+    }; 
 
     render() {
         const computedStyle = {
             backgroundColor: this.state.colour
         };
 
+        if(this.state.clicked) {
+            return (
+                   <div className="Colour-Selector-Clicked" style={computedStyle}>
+                        <div className="Clicked-Padding"/>
+                        <TwitterPicker 
+                            color={this.state.colour} 
+                            colors={CONSTS.COLOUR_SWATCH}
+                            onChangeComplete={this._colourChangeHandler} 
+                            onSwatchHover={this._colourHoverHandler} />
+                    </div> 
+            );
+        }
+
         return (
-            <div className="Colour-Selector" style={computedStyle}></div>
+            <div className="Colour-Selector" style={computedStyle} onClick={this._clickHandler}></div>
         );
     }
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,4 +5,6 @@ export default {
         H_MULTIPLE: 0.5,
         DEFAULT_COLOUR: "#659ed5",
     },
+
+    COLOUR_SWATCH: ['#FF6900', '#FCB900', '#7BDCB5', '#00D084', '#8ED1FC', '#0693E3', '#ABB8C3', '#EB144C', '#F78DA7', '#9900EF']
 }
