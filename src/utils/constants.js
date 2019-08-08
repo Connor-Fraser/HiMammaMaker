b612dd491478566ca87177fd690b7fc277ffac4a
Connor <connor.fraser4@gmail.com>
2019-08-08 17:15:40 -0400

    Move svg defs to constant file and remake drawing

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,12 +5,14 @@ export default {
         H_MULTIPLE: 0.5,
         DEFAULT_COLOUR: "#659ed5",
         COLOUR_SWATCH: ['#659ed5', '#FCB900', '#7BDCB5', '#00D084', '#8ED1FC', '#0693E3', '#ABB8C3', '#EB144C', '#F78DA7', '#9900EF'],
+        SVG: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 452.44 213.43"><defs><style>.cls-1{fill:FILL_COLOUR}</style></defs><title>SHIRT_SVG</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="cls-1" d="M452.44,155.05c0-72.6-66-155-157.91-155H281.91c0,24.29-24.93,44-55.69,44s-55.69-19.7-55.69-44H157.91C66,0,0,82.45,0,155.05H70.16v58.38h312V155.05Z"/></g></g></svg>`,
 
         CUFFS: {
             W_MULTIPLE: 0,
             H_MULTIPLE: 0.728,
             DEFAULT_COLOUR: "#476e93",
             COLOUR_SWATCH: ['#476e93', '#FCB900', '#7BDCB5', '#00D084', '#8ED1FC', '#0693E3', '#ABB8C3', '#EB144C', '#F78DA7', '#9900EF'],
+            SVG: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 289.61 13.41"><title>SHIRTCUFF_SVG</title><defs><style>.cls-1{fill:FILL_COLOUR}</style></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><rect class="cls-1" width="44.92" height="13.41"/><rect class="cls-1" x="244.69" width="44.92" height="13.41"/></g></g></svg>`
         }
     },
 
@@ -21,26 +23,31 @@ export default {
         NECK: {
             W_MULTIPLE: 0.35,
             H_MULTIPLE: 0.5,
+            SVG: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 100"><title>NECK_SVG</title><defs><style>.cls-1{fill:FILL_COLOUR}</style></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="cls-1" d="M38,0H0A28.65,28.65,0,0,0,28.65,28.65,28.66,28.66,0,0,0,57.31,0H38"/></g></g></svg>`
         },
 
         EARS: {
             W_MULTIPLE: 0.12,
             H_MULTIPLE: 0.27,
+            SVG: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 42.26"><title>EARS_SVG</title><defs><style>.cls-1{fill:FILL_COLOUR}</style></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><circle class="cls-1" cx="283.96" cy="21.13" r="21.13"/><circle class="cls-1" cx="21.13" cy="21.13" r="21.13"/></g></g></svg>`
         },
 
         FACE: {
             W_MULTIPLE: 0.175,
             H_MULTIPLE: 0.08,
+            SVG: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300"><title>FACE_SVG</title><defs><style>.cls-1{fill:FILL_COLOUR}</style></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><circle class="cls-1" cx="97.7" cy="97.7" r="97.7"/></g></g></svg>`
         },
 
         NOSE: {
             W_MULTIPLE: 0.45,
             H_MULTIPLE: 0.3,
+            SVG: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300"><title>NOSE_SVG</title><defs><style>.cls-1{fill:FILL_COLOUR}</style></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><polygon class="cls-1" points="0 27.2 15.7 0 31.41 27.2 0 27.2"/></g></g></svg>`
         },
 
         ARMS : {
             W_MULTIPLE: 0.031,
             H_MULTIPLE: .759,
+            SVG: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 291 300"><title>ARMS_SVG</title><defs><style>.cls-1{fill:FILL_COLOUR}</style></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><rect class="cls-1" width="36.4" height="23.97"/><rect class="cls-1" x="236.16" width="36.4" height="23.97"/></g></g></svg>`
         }
     }
 }
